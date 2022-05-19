5f4e0316699b20007a034947e7b15fa9a61cd3e4
Willy Jacquet <willy.jacquet@outlook.com>
2022-05-19 22:14:22 +0200

    Some progress on fed nothing to head

--- src/parsium/mckeeman/parser/head.hpp
+++ src/parsium/mckeeman/parser/head.hpp
@@ -3,6 +3,7 @@
 #include "cursor.hpp"
 
 #include <parsium/common/exception/precondition_violation.hpp>
+#include <parsium/common/std/vector/concatenation.hpp>
 #include <parsium/common/tag/undefined_behaviour.hpp>
 
 #include <iterator>
@@ -27,33 +28,51 @@ bool is_empty(const Head& h) {
 }
 
 inline
-Cursor& top_or(Head& h, common::UndefinedBehaviourTag) {
+void pop(Head& h) {
+	if(!is_empty(h)) {
+		h.nested_cursors.pop_back();
+	}
+}
+
+inline
+Cursor& top_or(Head& h, UndefinedBehaviourTag) {
 	return h.nested_cursors.back();
 }
 
 inline
 std::vector<Head> fed_nothing(const Grammar& g, Head h) {
-	if(is_empty(h)) {
-		return {};
-	} else {
-		auto result = std::vector<Head>();
-		auto& top = top_or(h, common::ub);
+	auto result = std::vector<Head>();
+	if(!is_empty(h)) {
+		auto& top = top_or(h, UB);
 		if(top.character_index == 0) {
 			// The cursor is not inside a literal,
 			// otherwise it has to be fed a symbol to progress.
 			auto& next_item = top.alternative->items[top.item_index];
 			if(const Name* name = name_or(next_item, nullptr)) {
-				if(rule(g, name).does_accept_nothing) {
+				auto& rule_ = rule(g, name);
+				if(rule_.does_accept_nothing) {
 					if(is_at_last_item(top)) {
 						// The cursor goes out of the alternative.
+						auto new_head = h;
+						pop(h);
+						result = fed_nothing(g, std::move(new_head));
 					} else {
-						
+						auto new_head = h;
+						top_or(new_head, UB).item_index += 1;
+						result = fed_nothing(g, std::move(new_head));
 					}
+				}
+				for(auto& alternative : rule_) {
+					auto alternative_head = h;
+					alternative_head.nested_cursors.push_back(cursor(alternative));
+					result = concatenation(std::move(result),
+						fed_nothing(g, std::move(alternative_head)));
 				}
 			}
 		}
-		return result;
 	}
+	result.emplace_back(std::move(h));
+	return result;
 }
 
 }
