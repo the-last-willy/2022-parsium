a0fde80e04c3bc183db0206efb935aec178bdb34
Willy Jacquet <willy.jacquet@outlook.com>
2022-05-22 21:50:32 +0200

    Simple test program for json parser

--- src/sandbox/mckeeman/main.cpp
+++ src/sandbox/mckeeman/main.cpp
@@ -5,45 +5,47 @@
 #include <iostream>
 #include <sstream>
 
+using namespace parsium;
 using namespace parsium::mckeeman;
 
-std::string format(const Head& h) {
-	auto format_ = std::stringstream() << ".";
-	if(!is_empty(h)) {
-		for(auto& cursor : h.nested_cursors) {
-			format_ << "/" << cursor.rule->name.string;
-			format_ << "/" << cursor.alternative;
-			format_ << "/" << cursor.item;
-			format_ << "/" << cursor.character;
+std::string format(const Grammar& g, const MultiHead& mh) {
+	auto ss = std::stringstream();
+	ss << "is_accepting = " << mh.is_accepting << ",\n";
+	ss << "heads = {\n";
+	for(auto& head : mh.heads) {
+		ss << "  ";
+		auto rule = mh.base_rule;
+		for(auto& cursor : head.nested_cursors) {
+			ss << rule->name.string << "/";
+			auto alternative_index = alternative_index_or(*rule, *cursor.alternative, _throw);
+			ss << alternative_index << "/" << cursor.item_index << "/";
+			auto& item = current_item(cursor);
+			if(auto literal = literal_or(item, nullptr)) {
+				ss << cursor.character_index;
+			} else if(auto name = name_or(item, nullptr)) {
+				rule = &mckeeman::rule(g, *name);
+			} else {
+				throw PreconditionViolation();
+			}
 		}
+		ss << "\n";
 	}
-	return format_.str();
+	ss << "}\n";
+	return ss.str();
 }
 
-void print(const Parser& p) {
-	std::cout << "is_accepting = " << (is_accepting(p) ? "true" : "false") << "\n";
-	std::cout << "is_halted    = " << (is_halted(p) ? "true" : "false") << "\n";
-	std::cout << "heads[" << size(p.heads) << "] = {\n";
-	for(auto& h : p.heads) {
-		(void) h;
-		std::cout << "  \"" << format(h) << "\"\n";
-	}
-	std::cout << "}\n";
+std::string format(const Parser& p) {
+	return format(p.grammar, p.multi_head);
 }
 
 int main() {
 	auto json_parser = parser(json_format(), Name("element"));
 
-	auto input = std::string_view("{}");
-
-	for(std::size_t i = 0; i < size(input); ++i)
-	{
-		std::cout << "read: " << input.substr(0, i + 1);
-		std::cout << std::endl;
-
-		feed(json_parser, input[i]);
-
-		print(json_parser);
-		std::cout << std::endl;
+	auto c = char();
+	std::cout << "> ";
+	while(std::cin >> c) {
+		json_parser.multi_head = fed(json_parser.grammar, json_parser.multi_head, c);
+		std::cout << format(json_parser) << std::endl;
+		std::cout << "> ";
 	}
 }
