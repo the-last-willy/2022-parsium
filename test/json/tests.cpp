3d3b416c6136473479bfc75eab59ac0a2c4f0265
Willy Jacquet <willy.jacquet@outlook.com>
2022-05-24 21:27:45 +0200

    Rewrote tests

--- test/json/tests.cpp
+++ test/json/tests.cpp
@@ -1,12 +1,99 @@
-#include "tests.hpp"
+#include <example/mckeeman/json.hpp>
+#include <parsium/mckeeman.hpp>
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-#include <iterator>
-
-using namespace parsium;
+#include <string_view>
 
 TEST_CASE("json") {
-	
+	SECTION("rule: \"json\"") {
+		auto parser = parsium::mckeeman::parser(
+			json_format(), parsium::mckeeman::Name("json"));
+		auto index = 1;
+		SECTION("#" + std::to_string(index++)) {
+			auto input = std::string_view("{}");
+			auto i = std::size_t(0);
+			for(; i < size(input); ++i) {
+				feed(parser, input[i]);
+				if(is_halted(parser)) {
+					break;
+				}
+			}
+			REQUIRE(is_accepting(parser));
+			REQUIRE(i == size(input));
+		}
+		SECTION("#" + std::to_string(index++)) {
+			auto input = std::string_view("[]");
+			auto i = std::size_t(0);
+			for(; i < size(input); ++i) {
+				feed(parser, input[i]);
+				if(is_halted(parser)) {
+					break;
+				}
+			}
+			REQUIRE(is_accepting(parser));
+			REQUIRE(i == size(input));
+		}
+		SECTION("#"  + std::to_string(index++)) {
+			auto input = std::string_view("\"\"");
+			auto i = std::size_t(0);
+			for(; i < size(input); ++i) {
+				feed(parser, input[i]);
+				if(is_halted(parser)) {
+					break;
+				}
+			}
+			REQUIRE(is_accepting(parser));
+			REQUIRE(i == size(input));
+		}
+		SECTION("#"  + std::to_string(index++)) {
+			auto input = std::string_view("0");
+			auto i = std::size_t(0);
+			for(; i < size(input); ++i) {
+				feed(parser, input[i]);
+				if(is_halted(parser)) {
+					break;
+				}
+			}
+			REQUIRE(is_accepting(parser));
+			REQUIRE(i == size(input));
+		}
+		SECTION("#" + std::to_string(index++)) {
+			auto input = std::string_view("\"true\"");
+			auto i = std::size_t(0);
+			for(; i < size(input); ++i) {
+				feed(parser, input[i]);
+				if(is_halted(parser)) {
+					break;
+				}
+			}
+			REQUIRE(is_accepting(parser));
+			REQUIRE(i == size(input));
+		}
+		SECTION("#" + std::to_string(index++)) {
+			auto input = std::string_view("\"false\"");
+			auto i = std::size_t(0);
+			for(; i < size(input); ++i) {
+				feed(parser, input[i]);
+				if(is_halted(parser)) {
+					break;
+				}
+			}
+			REQUIRE(is_accepting(parser));
+			REQUIRE(i == size(input));
+		}
+		SECTION("#" + std::to_string(index++)) {
+			auto input = std::string_view("\"null\"");
+			auto i = std::size_t(0);
+			for(; i < size(input); ++i) {
+				feed(parser, input[i]);
+				if(is_halted(parser)) {
+					break;
+				}
+			}
+			REQUIRE(is_accepting(parser));
+			REQUIRE(i == size(input));
+		}
+	}
 }
