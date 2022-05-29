9b32d32034ec164484f53d2e9b124626e4d4da93
Willy Jacquet <willy.jacquet@outlook.com>
2022-05-29 09:21:01 +0200

    Check if rule is accepting

--- src/parsium/mckeeman/grammar.hpp
+++ src/parsium/mckeeman/grammar.hpp
@@ -31,5 +31,33 @@ Rule& rule(Grammar& g, const Name& n) {
 	throw std::runtime_error("No rule with name.");
 }
 
+inline
+bool is_accepting(const Grammar& g, const Rule& r) {
+	if(r.does_accept_nothing) {
+		return true;
+	} else {
+		auto is_alternative_accepting = true;
+		for(auto& alternative : r.alternatives) {
+			is_alternative_accepting = true;
+			for(std::size_t i = 0; i < item_count(alternative); ++i) {
+				auto& item = alternative.items[i];
+				if(literal_or(item, nullptr)) {
+					is_alternative_accepting = false;
+					break;
+				} else if(auto name = name_or(item, nullptr)) {
+					if(!is_accepting(g, rule(g, *name))) {
+						is_alternative_accepting = false;
+						break;
+					}
+				}
+			}
+			if(is_alternative_accepting) {
+				break;
+			}
+		}
+		return is_alternative_accepting;
+	}
+}
+
 }
 }
