f981d82b1313fbf1f9293c6bc13ff5eb2181f6f8
Willy Jacquet <willy.jacquet@outlook.com>
2022-05-29 09:21:53 +0200

    Updated includes

--- src/parsium/mckeeman.hpp
+++ src/parsium/mckeeman.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "mckeeman/parser/multi_head/fed.hpp"
+#include "mckeeman/parser/parser/is_accepting.hpp"
 #include "mckeeman/parser/parser/is_halted.hpp"
+#include "mckeeman/parser/parser/feed.hpp"
 #include "mckeeman/parser/parser/parser.hpp"
 #include "mckeeman/grammar.hpp"
