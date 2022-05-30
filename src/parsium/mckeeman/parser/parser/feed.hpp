3f5e7bbf70a6d259a4e46631e8fec1dd15340ef8
Willy Jacquet <willy.jacquet@outlook.com>
2022-05-30 23:16:18 +0200

    Duplicated feed functions to take the parser as a parameter

--- src/parsium/mckeeman/parser/parser/feed.hpp
+++ src/parsium/mckeeman/parser/parser/feed.hpp
@@ -7,6 +7,51 @@ namespace parsium {
 namespace mckeeman {
 
 inline
+MultiHead fed(Parser& p, const Head& h, char symbol) {
+	if(h.nested_cursors.empty()) {
+		return {};
+	} else {
+		auto result = MultiHead();
+		auto& top = top_or(h, UB);
+		auto& item = current_item(top);
+		if(auto literal = literal_or(item, nullptr)) {
+			if(auto characters = characters_or(*literal, nullptr)) {
+				if((*characters)[h.character_index] == symbol) {
+					unify(result, moved_to_next_character(h));
+				}
+			} else if(auto range_exclude = range_exclude_or(*literal, nullptr)) {
+				if(does_accept(*range_exclude, symbol)) {
+					unify(result, moved_to_next_character(h));
+				}
+			} else if(auto singleton = singleton_or(*literal, nullptr)) {
+				if(does_accept(*singleton, symbol)) {
+					unify(result, moved_to_next_character(h));
+				}
+			}
+		} else if(auto name = name_or(item, nullptr)) {
+			auto& rule = mckeeman::rule(p.grammar, *name);
+			for(auto& alternative : rule.alternatives) {
+				auto nested = mckeeman::nested(h, alternative);
+				unify(result, fed(p.grammar, nested, symbol));
+			}
+			if(is_accepting(p.grammar, rule)) {
+				unify(result, fed(p.grammar, moved_to_next_item(h), symbol));
+			}
+		}
+		return result;
+	}
+}
+
+inline
+MultiHead fed(Parser& p, MultiHead& mh, char symbol) {
+	auto result = MultiHead();
+	for(auto& head : mh.heads) {
+		unify(result, fed(p.grammar, head, symbol));
+	}
+	return result;
+}
+
+inline
 void feed(Parser& p, char symbol) {
 	auto multi_head = MultiHead();
 	multi_head.base_rule = p.multi_head.base_rule;
