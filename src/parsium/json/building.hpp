d05cb51d7c6bc7cbab4e2ac90db0a63699221293
Willy Jacquet <willy.jacquet@outlook.com>
2022-06-21 22:24:59 +0200

    Updated includes

--- src/parsium/json/building.hpp
+++ src/parsium/json/building.hpp
@@ -2,11 +2,19 @@
 
 #include "building/array.hpp"
 #include "building/array/at.inl"
+#include "building/array/create_at_end.inl"
+#include "building/array/elements.inl"
+#include "building/array/is_valid.inl"
 #include "building/json.hpp"
+#include "building/json/is_valid.inl"
 #include "building/element.hpp"
+#include "building/element/is_valid.inl"
 #include "building/number.hpp"
+#include "building/number/is_valid.inl"
 #include "building/object.hpp"
+#include "building/object/is_valid.inl"
 #include "building/string.hpp"
+#include "building/string/is_valid.inl"
 
 /*******************************************************************************
 // Template file.
