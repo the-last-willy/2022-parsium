edde7d80d83e668e2b585f39b5f7aebb3e009bc0
Willy Jacquet <willy.jacquet@outlook.com>
2022-06-21 22:23:25 +0200

    Added a few member functions to array

--- src/parsium/json/building/array.hpp
+++ src/parsium/json/building/array.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <parsium/common/tag/shallow.hpp>
 #include <parsium/common/tag/undefined_behaviour.hpp>
 
 #include <memory>
@@ -17,6 +18,13 @@ class Array {
 public:
 	auto at_or(std::size_t i, decltype(UB)) const -> const Element&;
 	auto at_or(std::size_t i, decltype(UB)) -> Element&;
+
+	auto elements() const -> const std::vector<Element>&;
+	auto elements() -> std::vector<Element>&;
+
+	auto create_at_end() -> Element&;
+
+	auto is_valid(decltype(shallow)) const -> bool;
 };
 
 }}}
