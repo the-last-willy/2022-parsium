0069ae3f11a777e8200c1f78d7cfc23d78df92cf
Willy Jacquet <willy.jacquet@outlook.com>
2022-06-23 22:48:56 +0200

    Udpated includes

--- src/parsium/json/building.hpp
+++ src/parsium/json/building.hpp
@@ -15,21 +15,21 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "building/json.hpp"
+#include "parsium/json/building/json.hpp"
 
-#include "building/array/at.inl"
-#include "building/array/create_at_end.inl"
-#include "building/array/elements.inl"
-#include "building/array/is_valid.inl"
-#include "building/json/is_valid.inl"
-#include "building/element/is_valid.inl"
-#include "building/member/element.inl"
-#include "building/member/is_valid.inl"
-#include "building/member/string.inl"
-#include "building/number/is_valid.inl"
-#include "building/object/is_valid.inl"
-#include "building/object/members.inl"
-#include "building/string/is_valid.inl"
+#include "parsium/json/building/array/at.inl"
+#include "parsium/json/building/array/create_at_end.inl"
+#include "parsium/json/building/array/elements.inl"
+#include "parsium/json/building/array/is_valid.inl"
+#include "parsium/json/building/json/is_valid.inl"
+#include "parsium/json/building/element/is_valid.inl"
+#include "parsium/json/building/member/element.inl"
+#include "parsium/json/building/member/is_valid.inl"
+#include "parsium/json/building/member/string.inl"
+#include "parsium/json/building/number/is_valid.inl"
+#include "parsium/json/building/object/create_member_at_end.inl"
+#include "parsium/json/building/object/is_valid.inl"
+#include "parsium/json/building/object/members.inl"
 
 /*******************************************************************************
 // Template file.
