47ad6d996d05ed09c6adcaa89fd803ff848758e0
Willy Jacquet <willy.jacquet@outlook.com>
2022-06-28 23:16:00 +0200

    Partially implemented tree

--- src/parsium/common/container/tree/tree.hpp
+++ src/parsium/common/container/tree/tree.hpp
@@ -1,14 +1,33 @@
 #pragma once
 
 #include "parsium/common/container/tree/node.hpp"
+#include "parsium/common/container/tree/subtree.hpp"
 
 #include <vector>
 
 namespace parsium {
 
+template<typename T>
 class Tree {
+	std::vector<T> data_;
+	std::vector<TreeNode> nodes_;
+
 public:
-	std::vector<>
+	auto data(TreeDataIndex i) const -> const T& {
+		return data_[i.value()];
+	}
+
+	auto data(TreeDataIndex i) -> T& {
+		return data_[i.value()];
+	}
+
+	auto subtree(TreeNodeIndex i) const -> Subtree<const T> {
+		return Subtree(*this, i);
+	}
+
+	auto subtree(TreeNodeIndex i) -> Subtree<T> {
+		return Subtree(*this, i);
+	}
 };
 
 }
