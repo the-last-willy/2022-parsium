290ecec489dc0b451cbf787eaa11c7856b4839b8
Willy Jacquet <willy.jacquet@outlook.com>
2022-06-28 23:15:09 +0200

    Continued implementing Subtree

--- src/parsium/common/container/tree/subtree.hpp
+++ src/parsium/common/container/tree/subtree.hpp
@@ -1,31 +1,63 @@
 #pragma once
 
 #include "parsium/common/container/tree/node.hpp"
+#include "parsium/common/container/tree/node_index.hpp"
 
 #include <parsium/common/typing/const_if.hpp>
-
-#include <vector>
+#include <parsium/common/typing/without_const.hpp>
 
 namespace parsium {
 
+template<typename T>
 class Tree;
 
+template<typename T>
+class SubtreeConnections {
+
+public:
+
+};
+
 /**
- * Subtrees have pointer semantics.
- * Lifetime is tied to `tree_` and `tree_`.
+ * Pointer semantics.
+ * Lifetime is tied to `tree_'.
  */
 template<typename T>
 class Subtree {
 public:
-	using Tree = ConstIf<Tree, std::is_const_v<T>>;
+	using QualifiedTree = ConstIf<Tree<WithoutConst<T>>, std::is_const_v<T>>;
 
 private:
-	Tree* tree_;
+	QualifiedTree* tree_;
+	TreeNodeIndex node_;
 
 public:
-	Subtree(Tree* t, Node* n)
+	Subtree(QualifiedTree& qt, TreeNodeIndex ni)
+	: tree_(&qt)
+	, node_(ni)
+	{
+		// Node index validity not checked.
+	}
+
+	Subtree(const Subtree<WithoutConst<T>>& s)
+	: tree_(&s.tree())
+	, node_(s.node())
+	{}
+
+	auto connections() const -> SubtreeConnections<T>;
+	auto data() const -> T&;
+	auto node() const -> TreeNodeIndex { return node_; }
+	auto tree() const -> QualifiedTree&;
 };
 
+template<typename T>
+auto subtree(const Tree<T>& t, TreeNodeIndex i) -> Subtree<const T> {
+	return Subtree<const T>(t, i);
+}
 
+template<typename T>
+auto subtree(Tree<T>& t, TreeNodeIndex i) -> Subtree<const T> {
+	return Subtree<const T>(t, i);
+}
 
 }
