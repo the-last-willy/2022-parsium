b40955e2db6991e1b8a3c4b028e4f9df90741d11
Willy Jacquet <willy.jacquet@outlook.com>
2022-06-28 23:14:52 +0200

    Merged parent and children in the same container

--- src/parsium/common/container/tree/node.hpp
+++ src/parsium/common/container/tree/node.hpp
@@ -1,17 +1,15 @@
 #pragma once
 
+#include "parsium/common/container/tree/data_index.hpp"
 #include "parsium/common/container/tree/node_index.hpp"
 
 #include <vector>
 
 namespace parsium {
 
-template<typename Data>
-struct Node {
-	Data data;
-
-	NodeIndex parent;
-	std::vector<NodeIndex> children;
+struct TreeNode {
+	std::vector<TreeNodeIndex> connections;
+	TreeDataIndex data;
 };
 
 }
