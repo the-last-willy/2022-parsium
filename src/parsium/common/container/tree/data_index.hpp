73915eb3c5a960913cb55e37b6bc0258c4ace021
Willy Jacquet <willy.jacquet@outlook.com>
2022-06-29 22:38:51 +0200

    Added invalid data index

--- src/parsium/common/container/tree/data_index.hpp
+++ src/parsium/common/container/tree/data_index.hpp
@@ -1,16 +1,21 @@
 #pragma once
 
 #include <cstddef>
+#include <limits>
 
 namespace parsium {
 
+constexpr auto invalid_tree_data_index_value
+= std::numeric_limits<std::size_t>::max();
+
 class TreeDataIndex {
-	std::size_t value_ = 0;
+	std::size_t value_ = invalid_tree_data_index_value;
 
 public:
+	constexpr
 	TreeDataIndex() = default;
 
-	explicit
+	constexpr explicit
 	TreeDataIndex(std::size_t value)
 	: value_(value)
 	{}
@@ -19,5 +24,13 @@ public:
 		return value_;
 	}
 };
+
+inline
+bool is_valid(TreeDataIndex i) {
+	return i.value() != invalid_tree_data_index_value;
+}
+
+constexpr auto invalid_tree_data_index
+= TreeDataIndex(invalid_tree_data_index_value);
 
 }
