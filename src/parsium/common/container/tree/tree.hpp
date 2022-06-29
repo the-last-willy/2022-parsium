b1ed144a0d3c006b1e3f8821a28d16af27a480b5
Willy Jacquet <willy.jacquet@outlook.com>
2022-06-29 22:43:20 +0200

    Removed data vector, error handling, root construction and destruction functions

--- src/parsium/common/container/tree/tree.hpp
+++ src/parsium/common/container/tree/tree.hpp
@@ -3,30 +3,53 @@
 #include "parsium/common/container/tree/node.hpp"
 #include "parsium/common/container/tree/subtree.hpp"
 
+#include <parsium/common/tag/undefined_behaviour.hpp>
+
 #include <vector>
 
 namespace parsium {
 
 template<typename T>
 class Tree {
-	std::vector<T> data_;
-	std::vector<TreeNode> nodes_;
+	std::vector<TreeNode<T>> nodes_;
+	TreeNodeIndex root_ = invalid_tree_node_index;
 
 public:
-	auto data(TreeDataIndex i) const -> const T& {
-		return data_[i.value()];
+
+	// Access.
+
+	auto data_or(decltype(UB), TreeDataIndex i) const -> const T& {
+		auto node_index = TreeNodeIndex(i.value());
+		return nodes_[node_index.value()].data;
 	}
 
-	auto data(TreeDataIndex i) -> T& {
-		return data_[i.value()];
+	auto data_or(decltype(UB), TreeDataIndex i) -> T& {
+		auto node_index = TreeNodeIndex(i.value());
+		return nodes_[node_index.value()].data;
 	}
 
-	auto subtree(TreeNodeIndex i) const -> Subtree<const T> {
+	auto root_index() const -> TreeNodeIndex {
+		return root_;
+	}
+
+	auto subtree_or(decltype(UB), TreeNodeIndex i) const -> Subtree<const T> {
 		return Subtree(*this, i);
 	}
 
-	auto subtree(TreeNodeIndex i) -> Subtree<T> {
+	auto subtree_or(decltype(UB), TreeNodeIndex i) -> Subtree<T> {
 		return Subtree(*this, i);
+	}
+
+	//
+
+	auto construct_root_or(decltype(UB), T&& data) -> Subtree<T> {
+		root_ = TreeNodeIndex(nodes_.size());
+		nodes_.push_back(TreeNode(root_, std::move(data)));
+		return subtree(*this, root_);
+	}
+
+	auto destroy_root() -> void {
+		nodes_.clear();
 	}
 };
 
