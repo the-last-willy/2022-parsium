2da7a8254d723dd09614ca0c21573139f758e8a9
Willy Jacquet <willy.jacquet@outlook.com>
2022-06-29 22:39:28 +0200

    Data is now inside the node and get parent of node

--- src/parsium/common/container/tree/node.hpp
+++ src/parsium/common/container/tree/node.hpp
@@ -3,13 +3,25 @@
 #include "parsium/common/container/tree/data_index.hpp"
 #include "parsium/common/container/tree/node_index.hpp"
 
+#include <parsium/common/tag/self.hpp>
+
 #include <vector>
 
 namespace parsium {
 
+template<typename T>
 struct TreeNode {
-	std::vector<TreeNodeIndex> connections;
-	TreeDataIndex data;
+	std::vector<TreeNodeIndex> connections_;
+	T data_;
+
+	TreeNode(TreeNodeIndex parent, T&& data)
+	: connections_({parent})
+	, data_(std::move(data))
+	{}
+
+	auto parent_or(decltype(self)) const -> TreeNodeIndex {
+		return connections_.front();
+	}
 };
 
 }
