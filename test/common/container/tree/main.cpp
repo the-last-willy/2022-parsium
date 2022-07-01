011d6ef184d183731899bb2ac44cfced30764093
Willy Jacquet <willy.jacquet@outlook.com>
2022-07-01 23:42:27 +0200

    More tree tests

--- test/common/container/tree/main.cpp
+++ test/common/container/tree/main.cpp
@@ -3,30 +3,47 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-TEST_CASE("Default ctor") {
-	parsium::Tree<int>();
+using namespace parsium;
+
+TEST_CASE("Simple case: binary tree") {
+	auto tree = Tree<int>();
+
+	REQUIRE(!tree.root_or(nullptr));
 }
 
-TEST_CASE("Empty tree") {
-	auto tree = parsium::Tree<int>();
+TEST_CASE("Simple case: empty tree") {
+	auto tree = Tree<int>();
 
-	SECTION("The tree has no root") {
-		REQUIRE(!tree.root_or(nullptr));
-	}
+	REQUIRE(!tree.root_or(nullptr));
 }
 
-TEST_CASE("Root tree") {
-	auto tree = parsium::Tree<int>();
-	auto root = tree.construct_root_or(parsium::UB, 1);
+TEST_CASE("Simple case: root tree") {
+	auto data = 123456;
+	auto tree = Tree<int>();
+	auto root = tree.construct_root_or(UB, data);
 
-	SECTION("The root has no child") {
-
-	}
-	SECTION("The root has no parent") {
-		REQUIRE(!root.parent_or(nullptr));
-	}
+	REQUIRE(root.data() == data);
+	REQUIRE(root.child_count() == 0);
+	REQUIRE(!root.parent_or(nullptr));
 }
 
-TEST_CASE("Mutable tree_node to const tree_node conversion") {
+TEST_CASE("Simple case: unary tree") {
+	TreeNodePtr<int> a;
+	FakePtr<TreeNodeRef<int>> b = a.f();
 
+
+//	auto tree = parsium::Tree<int>();
+//
+//	TreeNodePtr<int> node = &tree.construct_root_or(UB, 0);
+//
+//	for(int i = 1; i < 4; ++i) {
+//		node = &(*node).add_child(i);
+//	}
+//
+//	node = tree.root_or(nullptr);
+//
+//	for(int i = 0; i < 4; ++i) {
+//		REQUIRE(node);
+//		// REQUIRE(node->child_count() == (i < 3 ? 1 : 0));
+//	}
 }
