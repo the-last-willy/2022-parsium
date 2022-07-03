7b05f02ad36823bd8ccb46a98e8282f56afb3a35
Willy Jacquet <willy.jacquet@outlook.com>
2022-07-03 21:50:57 +0200

    A bit of testing for naive tree

--- test/common/container/naive_tree/main.cpp
+++ test/common/container/naive_tree/main.cpp
@@ -6,7 +6,14 @@
 using namespace parsium;
 
 TEST_CASE("Binary tree") {
+	constexpr auto depth = 2;
 	auto tree = NaiveTree<int>();
+	{ // Construction.
+
+	}
+	SECTION("Validation") {
+
+	}
 }
 
 TEST_CASE("Empty tree") {
@@ -16,17 +23,40 @@ TEST_CASE("Empty tree") {
 
 TEST_CASE("Pair tree") {
 	auto tree = NaiveTree<int>();
+	{ // Construction.
+		auto first = tree.construct_root(1);
+		auto second = first->add_child(2);
+		{ // Validates first.
+			REQUIRE(first->child_count() == 1);
+			REQUIRE(*first->data() == 1);
+		} { // Validates second.
+			REQUIRE(second->child_count() == 0);
+			REQUIRE(*second->data() == 2);
+		}
+	}
+	SECTION("Destroy second") {
+		auto first = tree.root();
+		first->remove_child(0);
+		{ // Validates first.
+			REQUIRE(first->child_count() == 0);
+			REQUIRE(*first->data() == 1);
+		}
+	}
 }
 
 TEST_CASE("Singleton tree") {
 	auto data = 123456;
 	auto tree = NaiveTree<int>();
-	tree.construct_root(data);
-	REQUIRE(tree.has_root());
-	auto& root = tree.root();
-	REQUIRE(!root.has_parent());
-	REQUIRE(root.child_count() == 0);
-	REQUIRE(root.data() == data);
+	{ // Construction.
+		tree.construct_root(data);
+	}
+	SECTION("Validation") {
+		REQUIRE(tree.has_root());
+		auto root = tree.root();
+		REQUIRE(!root->has_parent());
+		REQUIRE(root->child_count() == 0);
+		REQUIRE(*root->data() == data);
+	}
 }
 
 TEST_CASE("Unary tree") {
@@ -34,22 +64,22 @@ TEST_CASE("Unary tree") {
 	auto tree = NaiveTree<int>();
 	NaiveTreeNode<int>* node = nullptr;
 	{ // Constructing the tree.
-		node = &tree.construct_root(0);
+		node = tree.construct_root(0);
 		for(int i = 1; i < depth; ++i) {
-			node = &node->add_child(i);
-			node->data() = i;
+			node = node->add_child(i);
+			*node->data() = i;
 			REQUIRE(node->has_parent());
 		}
 	} { // Traversing the tree.
-		node = &tree.root();
+		node = tree.root();
 		for(int i = 0; i < depth - 1; ++i) {
 			REQUIRE(node->child_count() == 1);
-			REQUIRE(node->data() == i);
-			node = &node->child(0);
+			REQUIRE(*node->data() == i);
+			node = node->child(0);
 		}
 		{ // Last node.
 			REQUIRE(node->child_count() == 0);
-			REQUIRE(node->data() == depth - 1);
+			REQUIRE(*node->data() == depth - 1);
 		}
 	}
 
