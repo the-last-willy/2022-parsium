ddefa2b09f71c86a7e47e38f2072f748379b1e92
Willy Jacquet <willy.jacquet@outlook.com>
2022-08-25 18:18:31 +0200

    Added force inline macro

---
+++ src/parsium/common/compiler/inlining_epilogue.hpp
@@ -0,0 +1,3 @@
+// No inclusion guard.
+
+#undef FORCE_INLINE
---
+++ src/parsium/common/compiler/inlining_prologue.hpp
@@ -0,0 +1,7 @@
+// No inclusion guard.
+
+#if(defined(_MSC_VER))
+	#define FORCE_INLINE __force_inline
+#else
+	#define FORCE_INLINE
+#endif
